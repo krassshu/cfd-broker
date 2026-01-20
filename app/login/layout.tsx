62f47502e2254ea4ec74cffb9d0a9c93686fd604
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2026-01-20 23:12:58 +0100

    background animations added

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -8,20 +8,20 @@ export const metadata: Metadata = {
 
 export default function Layout({children,}: Readonly<{ children: React.ReactNode }>) {
     return (
-        <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-gray-900/40">
+        <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-gray-900/50">
             <div className="absolute inset-0 -z-10">
                 <Image
                     src="/cryptobg.png"
                     alt="Crypto Background"
                     fill
                     priority
-                    className="object-cover"
+                    className="object-cover animate-pan"
                 />
             </div>
 
             {children}
 
-            <p className="mx-10 mt-5 text-center text-xs text-gray-300">
+            <p className="mx-10 mt-5 text-center text-xs text-gray-400 relative z-10">
                 CFDs and Options are complex instruments and come with a high risk of losing
                 money rapidly due to leverage. 75% of retail investor accounts lose money
                 when trading CFDs with this provider. You should consider whether you
